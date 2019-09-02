089b504f553526a3606c621b79769483cf503228
NewProjects <newprojects@manishs-mbp.lan>
2019-09-01 23:32:43 -0700

    All works including clicking on links to get to rooms

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, session, render_template, request, flash 
 from flask import redirect, session, abort, url_for,session,logging,request 
-from flask_socketio import SocketIO, join_room, emit
+from flask_socketio import SocketIO, join_room, emit, disconnect
 
 
 app = Flask(__name__)
@@ -12,29 +12,29 @@ socketio = SocketIO(app)
 app.config['SESSION_TYPE']	 = 'filesystem'
 app.secret_key 				 = os.urandom(12)
 
-channel_list = []
-i = 0
+ 
+i=-1
+channel_list = [] 
+error = ""
 
 
 #Base 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def index():
-	if session.get('logged_in'):
+ 	if session.get('logged_in'):
 		return render_template("channels.html")
 	else:
 		return redirect(url_for('dp_set'))
-
 
 
 @app.route("/dp_set", methods=["GET", "POST"]) 
 def dp_set():
 	if request.method == "POST":
 		dp_name = request.form["dp_name"]
- 		if dp_name != '':
+		if dp_name != '':
 			session['logged_in'] = True
 			session['dp_name']  = dp_name 
 			return render_template("channels.html")
-
 	return render_template("dp_set.html")
 
 
@@ -42,25 +42,39 @@ def dp_set():
 def channels():
 	if request.method == "POST":
 		return render_template("channels.html")
-
 	return render_template("dp_set.html")
 
+@socketio.on('startup')
+def on_startup():
+	global i, channel_list
+
+	if i>-1:
+		emit('master_channel_list', { "master_channel_list" : channel_list, "items" : i}, broadcast=True)
  
-@app.route("/logout")
+@socketio.on('add channel')
+def on_add_channel(channel_name):
+	global i, channel_list, error
+
+	error=""
+	if channel_name in channel_list:
+		error = "Channel already exists!"
+		emit('error',{"error_msg" : error}, broadcast=True)
+	else:
+		i = i+1
+		channel_list.append(channel_name)
+ 		emit('current_channel_list', { "ch_list" : channel_list, "items" : i}, broadcast=True)
+
+@app.route("/join",  methods=["GET", "POST"])
+def join_room():
+	 
+	return render_template("chats.html")
+
+
+@app.route("/logout",  methods=["GET", "POST"])
 def logout():
 	session.pop('username', None)
 	session['logged_in'] = False
 	return redirect(url_for('index'))
-
-@socketio.on('connect')
-def on_connect():
-	print("CONNECTED YES!")
-
-
-@socketio.on('create')
-def on_create(channel_name):
-	print("CONNECTED HERE!")
-	emit('join_room', channel_name)
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -1,13 +1,14 @@
 
  document.addEventListener('DOMContentLoaded', () => {
-  
-    
 
     // By default, submit button is disabled
     document.querySelector('#submit').disabled = true;
 
     // Enable button only if there is text in the input field
     document.querySelector('#channel').onkeyup = () => {
+
+        document.querySelector('#error_message').innerHTML = ""
+
         if (document.querySelector('#channel').value.length > 0)
             document.querySelector('#submit').disabled = false;
         else
@@ -16,27 +17,39 @@
 
     document.querySelector('#new-channel').onsubmit = () => {
 
- 
         // Create new item for list
-        const li = document.createElement('li');
-        li.innerHTML = document.querySelector('#channel').value;
+        //const li = document.createElement('li');
+        //li.innerHTML = document.querySelector('#channel').value;
 
-        // Add new item to channel lis
+        var channel_name = document.getElementById("channel").value;
 
-        document.querySelector('#channels').append(li);
+        socket.emit('add channel', channel_name);
 
+        socket.on('current_channel_list', function(data) {
+            j=0; display_list=""
+            while (j <= data.items) {
+                display_list += "<li> <a href=\"/join\">"+data.ch_list[j]+"</li>"
+                j++
+            }
+            document.querySelector('#channels').innerHTML = display_list;
+        });
+   
         // Clear input field and disable button again
         document.querySelector('#channel').value = '';
         document.querySelector('#submit').disabled = true;
 
-        socket.emit('create', li.innerHTML);
-        
         // Stop form from submitting
         return false;
     };
 
-
+    socket.on('error', function(data) {
+         if (data.error_msg != "") {
+            document.querySelector('#error_message').innerHTML = data.error_msg;
+        }
+    });
+ 
+ 
 });
 
 
-     
+        
